2f141a0fc5143783a1e7b326d99ccd04642adcd3
Andreas Linz <klingt.net@gmail.com>
2019-01-03 17:34:40 +0100

    Remove upower comment
    ... and a missing space :D

--- panel.sh
+++ panel.sh
@@ -7,7 +7,6 @@ _USER="$(whoami)"
 _HOST="$(hostname)"
 _WIFI="$(nmcli --terse --fields name connection show --active | xargs | tr ' ' ,)"
 _UPTIME="$(uptime --pretty)"
-_IP="$(ip -json route show scope link | jq --raw-output '.[].prefsrc' | xargs| tr ' ' ,)"
-#_BAT="$(upower --show-info)"
+_IP="$(ip -json route show scope link | jq --raw-output '.[].prefsrc' | xargs | tr ' ' ,)"
 
 echo "${_USER}@${_HOST} | $_UPTIME | connected to $_WIFI | $_IP | $_DATE"
