7928096ddb3f48d770d98ac26f958f69a46b0f99
Andreas Linz <klingt.net@gmail.com>
2019-01-03 18:16:13 +0100

    Show battery level (if present)

--- panel.sh
+++ panel.sh
@@ -8,5 +8,7 @@ _HOST="$(hostname)"
 _WIFI="$(nmcli --terse --fields name connection show --active | xargs | tr ' ' ,)"
 _UPTIME="$(uptime --pretty)"
 _IP="$(ip -json route show scope link | jq --raw-output '.[].prefsrc' | xargs | tr ' ' ,)"
+_BAT="$(for bat in $(ls /sys/class/power_supply); do [[ -e "/sys/class/power_supply/${bat}/capacity" ]] && echo ${bat} $(cat "/sys/class/power_supply/${bat}/capacity")\%; done)"
+[[ -z ${_BAT} ]] && _BAT='AC'
 
-echo "${_USER}@${_HOST} | $_UPTIME | connected to $_WIFI | $_IP | $_DATE"
+echo "${_USER}@${_HOST} | $_UPTIME | power: $_BAT | connected to $_WIFI | $_IP | $_DATE"
