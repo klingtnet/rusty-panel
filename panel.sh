ed77d6e5fe5b55d3bd6f21689e3d004ae6b93cc5
Andreas Linz <klingt.net@gmail.com>
2019-01-06 13:14:45 +0100

    Show public IP

--- panel.sh
+++ panel.sh
@@ -8,7 +8,8 @@ _HOST="$(hostname)"
 _WIFI="$(nmcli --terse --fields name connection show --active | tr ' ' _ | xargs | tr ' ' ,)"
 _UPTIME="$(uptime --pretty)"
 _IP="$(ip -json route show scope link | jq --raw-output '.[].prefsrc' | xargs | tr ' ' ,)"
+_PUBLIC_IP="$(curl -sf https://httpbin.org/ip | jq --raw-output .origin)"
 _BAT="$(for bat in $(ls /sys/class/power_supply); do [[ -e "/sys/class/power_supply/${bat}/capacity" ]] && echo ${bat} $(cat "/sys/class/power_supply/${bat}/capacity")\%; done)"
 [[ -z ${_BAT} ]] && _BAT='AC'
 
-echo "${_USER}@${_HOST} | $_UPTIME | power: $_BAT | connected to $_WIFI | $_IP | $_DATE"
+echo "${_USER}@${_HOST} | $_UPTIME | power: $_BAT | connected to $_WIFI | $_IP public: $_PUBLIC_IP | $_DATE"
