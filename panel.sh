7fc82843c6853008e60e0de7a4a7165c1fd5d62f
Andreas Linz <klingt.net@gmail.com>
2019-01-06 12:27:12 +0100

    Fix network names containing spaces
    First replace spaces with underscores and then concatenating them with
    commas.

--- panel.sh
+++ panel.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 _DATE="$(date --rfc-3339=seconds)"
 _USER="$(whoami)"
 _HOST="$(hostname)"
-_WIFI="$(nmcli --terse --fields name connection show --active | xargs | tr ' ' ,)"
+_WIFI="$(nmcli --terse --fields name connection show --active | tr ' ' _ | xargs | tr ' ' ,)"
 _UPTIME="$(uptime --pretty)"
 _IP="$(ip -json route show scope link | jq --raw-output '.[].prefsrc' | xargs | tr ' ' ,)"
 _BAT="$(for bat in $(ls /sys/class/power_supply); do [[ -e "/sys/class/power_supply/${bat}/capacity" ]] && echo ${bat} $(cat "/sys/class/power_supply/${bat}/capacity")\%; done)"
